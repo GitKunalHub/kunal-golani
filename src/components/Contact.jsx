6ac458a70bc92f8babe1fc2ec2faaf6d8a423130
GitKunalHub <kunalgolani01@gmail.com>
2023-11-30 17:22:05 +0530

    Built email functionality with emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,48 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    //6iNZtGSRIKTt3bSCd
+    //template_xyxl7mh
+    //service_aklqvzc
+
+    emailjs
+      .send(
+        "service_aklqvzc",
+        "template_xyxl7mh",
+        {
+          from_name: form.name,
+          to_name: "Kunal",
+          from_email: form.email,
+          to_email: "kunalgolani.work@gmail.com",
+          message: form.message,
+        },
+        "6iNZtGSRIKTt3bSCd"
+      )
+      .then(() => {
+        setLoading(false);
+        alert("Thank you. I will get back to you as soon as possible.");
+        setForm(
+          {
+            name: "",
+            email: "",
+            message: "",
+          },
+          (error) => {
+            setLoading(false);
+            console.log(error);
+            alert("Something went wrong.");
+          }
+        );
+      });
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
